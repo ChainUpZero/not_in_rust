29afbd08c2191928d4ae4b129783d7945d3d5bf4
isaacsue <805890851@qq.com>
2024-03-29 17:01:03 +0800

    delete unused "use"

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use scalar::Scalar;
-use ff::PrimeField;
 
 mod small_ntt;
 mod ntt_381;
 
 fn main() -> Result<(), String> {
-    // for n in (3..=33).step_by(6) {
-    //     let input: Vec<u64> = (0..=n).step_by(3).collect();
-    //     println!("input vec  = {:?}", input);
-    // 
-    //     let output = small_ntt::ntt(&input)?;
-    //     println!("after ntt  = {:?}", output);
-    //     assert_ne!(input, output);
-    // 
-    //     let original = small_ntt::i_ntt(&output)?;
-    //     println!("after inverse ntt = {:?}\n\n", original);
-    //     assert_eq!(input, original);
-    // }
+    for n in (3..=33).step_by(6) {
+        let input: Vec<u64> = (0..=n).step_by(3).collect();
+        println!("input vec  = {:?}", input);
+    
+        let output = small_ntt::ntt(&input)?;
+        println!("after ntt  = {:?}", output);
+        assert_ne!(input, output);
+    
+        let original = small_ntt::i_ntt(&output)?;
+        println!("after inverse ntt = {:?}\n\n", original);
+        assert_eq!(input, original);
+    }
 
 
     let input = vec![Scalar::from(3), Scalar::from(3)];
