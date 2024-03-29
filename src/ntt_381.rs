0d2cdc70b1cd5df6d8af5b3628345bb2217dcca6
isaacsue <805890851@qq.com>
2024-03-29 16:58:04 +0800

    delete small ntt comments in 381 ntt

--- src/ntt_381.rs
+++ src/ntt_381.rs
@@ -1,25 +1,6 @@
 use scalar::{Scalar};
 use ff::{PrimeField};
 
-/*
-fn ntt(elements: &[u64]) -> Result<Vec<u64>, String> {
-    let n = elements.len() as u64;
-    let modulus = crate::small_ntt::find_modulus(elements)?;
-    let omega = crate::small_ntt::find_primitive_root(n, modulus)?;
-    Ok(crate::small_ntt::dft_matrix(n)
-        .iter()
-        .map(|row| {
-            elements
-                .iter()
-                .zip(row)
-                .map(|(elem, ij)| elem * omega.pow_mod(*ij, modulus))
-                .sum::<u64>()
-                % modulus
-        })
-        .collect::<Vec<u64>>())
-}
- */
-
 pub fn ntt_381(elements: &Vec<Scalar>) -> Vec<Scalar> {
     let n = elements.len() as u64;
     let mut matrix: Vec<Vec<u64>> = vec![vec![0; n as usize]; n as usize];
@@ -42,25 +23,6 @@ pub fn ntt_381(elements: &Vec<Scalar>) -> Vec<Scalar> {
         }).collect::<Vec<Scalar>>()
 }
 
-/*
-pub fn i_ntt(elements: &[u64]) -> Result<Vec<u64>, String> {
-    let n = elements.len() as u64;
-    let modulus = find_modulus(elements)?;
-    let omega = find_primitive_root(n, modulus)?;
-    Ok(dft_matrix(n)
-        .iter()
-        .map(|row| {
-            elements
-                .iter()
-                .zip(row)
-                .map(|(elem, ij)| elem * omega.inv_pow_mod(*ij, modulus))
-                .sum::<u64>()
-                * n.inv_pow_mod(1, modulus)
-                % modulus
-        })
-        .collect::<Vec<u64>>())
-}
- */
 pub fn i_ntt_381(elements: &Vec<Scalar>) -> Vec<Scalar> {
     let n = elements.len() as u64;
     let mut matrix: Vec<Vec<u64>> = vec![vec![0; n as usize]; n as usize];
