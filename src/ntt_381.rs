6e1249e1a9be2f29c09076df4d708d442668dcf7
isaacsue <805890851@qq.com>
2024-04-07 21:47:21 +0800

    change type invert to bit op

--- src/ntt_381.rs
+++ src/ntt_381.rs
@@ -1,45 +1,44 @@
-use scalar::Scalar;
 use ff::PrimeField;
+use scalar::Scalar;
 
 pub fn ntt_381(elements: &Vec<Scalar>) -> Vec<Scalar> {
     let n = elements.len() as u64;
     let mut matrix: Vec<Vec<u64>> = vec![vec![0; n as usize]; n as usize];
-    (0..n)
-        .for_each(|x| (0..n)
-            .for_each(
-                |y| matrix[x as usize][y as usize] = x * y));
-    // println!("{:#?}", matrix);
+    (0..n).for_each(|x| (0..n).for_each(|y| matrix[x as usize][y as usize] = x * y));
     matrix
         .iter()
         .map(|row| {
             elements
                 .iter()
                 .zip(row)
-                .map(|(elem, ij)|
-                    elem * Scalar::ROOT_OF_UNITY
-                        .pow(&[*ij * ((u32::MAX as u64 + 1) / n), 0, 0, 0])
-                )
+                .map(|(elem, ij)| {
+                    elem * Scalar::ROOT_OF_UNITY.pow(&[*ij * ((1u64 << 32) / n), 0, 0, 0])
+                })
                 .sum::<Scalar>()
-        }).collect::<Vec<Scalar>>()
+        })
+        .collect::<Vec<Scalar>>()
 }
 
 pub fn i_ntt_381(elements: &Vec<Scalar>) -> Vec<Scalar> {
     let n = elements.len() as u64;
     let mut matrix: Vec<Vec<u64>> = vec![vec![0; n as usize]; n as usize];
-    (0..n)
-        .for_each(|x| (0..n)
-            .for_each(
-                |y| matrix[x as usize][y as usize] = x * y));
+    (0..n).for_each(|x| (0..n).for_each(|y| matrix[x as usize][y as usize] = x * y));
     matrix
         .iter()
         .map(|row| {
             elements
                 .iter()
                 .zip(row)
-                .map(|(elem, ij)|
-                    elem * Scalar::ROOT_OF_UNITY_INV
-                        .pow(&[*ij * ((u32::MAX as u64 + 1) / n), 0, 0, 0])
-                )
-                .sum::<Scalar>() * Scalar::from(n).invert().unwrap()
-        }).collect::<Vec<Scalar>>()
+                .map(|(elem, ij)| {
+                    elem * Scalar::ROOT_OF_UNITY_INV.pow(&[
+                        *ij * ((1u64 << 32) / n),
+                        0,
+                        0,
+                        0,
+                    ])
+                })
+                .sum::<Scalar>()
+                * Scalar::from(n).invert().unwrap()
+        })
+        .collect::<Vec<Scalar>>()
 }
